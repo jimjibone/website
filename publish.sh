088b838855e6e9965828355a779ab3aa3a0e8b91
James Reuss <jamesreuss@gmail.com>
2016-06-06 18:40:31 +0100

    remove old build scripts

--- publish.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-cd themes/timeline && npm run build && cd ../..
-hugo --baseURL=https://jamesreuss.co.uk/ \
-     --destination="public"
--- serve-dev.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-hugo server --baseURL=https://jamesreuss.co.uk/dev/ \
-            --appendPort=false \
-            --port=8080 \
-            --disableLiveReload=true \
-            --destination="dev" \
-            --buildDrafts=true \
-            --renderToDisk
